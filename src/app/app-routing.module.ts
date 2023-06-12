051b29c4b8439b649c0dd3f147c48660f85595cb
Karen Coimes <daniz_18_8@hotmail.com>
2023-06-12 12:21:14 -0500

    implemento hashStrategy y Lazzy loading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
@@ -24,7 +24,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
+    useHash: true,
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
